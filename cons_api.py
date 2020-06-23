75988f6b9b3fd35e902f47d01e02e1555d12690f
Vitor Morais Siqueira <42281886+vitormsiqueira@users.noreply.github.com>
2020-06-23 15:39:33 -0400

    Consuming api request detail movie

--- cons_api.py
+++ cons_api.py
@@ -3,4 +3,12 @@ import requests
 from pprint import pprint
 
 print (' Consulta MOVIES \n')
+base_url = 'https://api.themoviedb.org/3/'
+api_key = '00fc77df8f735fe4ed9a21dfed246b1d'
 
+id_movie = '155'
+
+movies_details = requests.get(base_url+'movie/'+id_movie+'?api_key='+api_key+'&language=en-US')
+print(movies_details.status_code)
+
+print(movies_details.json())
